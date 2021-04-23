54d7866164428ddb59cbf554a62e331059fe2403
dom-hydracor-intern <domonique.walcott@hydracor.net>
2021-04-23 16:01:59 +0000

    simple REST steps

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -14,7 +14,7 @@ class ArticlesController extends AppController
     public function initialize(): void
     {
         parent::initialize();
-
+        $this->loadComponent('RequestHandler'); // Include the RequestHandler
         $this->loadComponent('Flash'); // Include the FlashComponent
     }
 
@@ -25,9 +25,17 @@ class ArticlesController extends AppController
      */
     public function index()
     {
+
+        $allarticles = $this->Articles->find('all')->all();
+        $this->set('articles', $allarticles);
+        $this->viewBuilder()->setOption('serialize', ['articles']);
+
+    /*
         $articles = $this->paginate($this->Articles);
 
         $this->set(compact('articles'));
+
+    */
     }
 
     /**
@@ -39,11 +47,19 @@ class ArticlesController extends AppController
      */
     public function view($id = null)
     {
+        $article = $this->Articles->get($id);
+        $this->set('article', $article);
+        $this->viewBuilder()->setOption('serialize', ['article']);
+
+    /*
+
         $article = $this->Articles->get($id, [
             'contain' => [],
         ]);
 
         $this->set(compact('article'));
+
+    */
     }
 
     /**
@@ -53,6 +69,22 @@ class ArticlesController extends AppController
      */
     public function add()
     {
+        $this->request->allowMethod(['post', 'put']);
+        $article = $this->Articles->newEntity($this->request->getData());
+        if ($this->Articles->save($article)) {
+            $this->Flash->success(__('Your article has been saved.'));
+        }
+        else 
+        {
+            $this->Flash->error(__('Unable to add your article.'));
+        }
+
+        $this->set([
+            'article' => $article,
+        ]);
+        $this->viewBuilder()->setOption('serialize', ['article']);
+
+    /*
         $article = $this->Articles->newEmptyEntity();
         if ($this->request->is('post')) {
             $article = $this->Articles->patchEntity($article, $this->request->getData());
@@ -68,6 +100,8 @@ class ArticlesController extends AppController
         // one category for an article
         $categories = $this->Articles->Categories->find('treeList')->all();
         $this->set(compact('categories'));
+
+    */
     }
 
     /**
@@ -79,6 +113,26 @@ class ArticlesController extends AppController
      */
     public function edit($id = null)
     {
+
+        $this->request->allowMethod(['patch', 'post', 'put']);
+        $article = $this->Article->get($id);
+        $article = $this->Article->patchEntity($article, $this->request->getData());
+        if ($this->Articles->save($article)) {
+            $this->Flash->success(__('Your article has been updated.'));
+
+        }
+        else 
+        {
+            $this->Flash->error(__('Unable to update your article.'));
+        }
+        $this->set([
+            'article' => $article,
+        ]);
+        $this->viewBuilder()->setOption('serialize', ['article']);
+
+
+    /*
+
         $article = $this->Articles->get($id, [
             'contain' => [],
         ]);
@@ -92,6 +146,8 @@ class ArticlesController extends AppController
             $this->Flash->error(__('Unable to update your article.'));
         }
         $this->set('article', $article);
+
+    */
     }
 
     /**
@@ -103,6 +159,19 @@ class ArticlesController extends AppController
      */
     public function delete($id = null)
     {
+        $this->request->allowMethod(['delete']);
+        $article = $this->Articles->get($id);
+
+        if(!$this->Articles->delete($article)){
+            $this->Flash->success(__('Your article has been updated.'));
+
+        }
+        else 
+        {
+            $this->Flash->error(__('Unable to update your article.'));
+        }
+
+    /*
         $this->request->allowMethod(['post', 'delete']);
         $article = $this->Articles->get($id);
         if ($this->Articles->delete($article)) {
@@ -110,5 +179,6 @@ class ArticlesController extends AppController
         } else {
             $this->Flash->error(__('The article could not be deleted. Please, try again.'));
         }
+    */
     }
 }
