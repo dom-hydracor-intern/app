4530b99f536a5d8c1d930cf70bcdbf8f48f84d1c
dom-hydracor-intern <domonique.walcott@hydracor.net>
2021-05-14 18:39:32 +0000

    toggle unauthenticated access

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -34,7 +34,7 @@ class AppController extends Controller
         parent::beforeFilter($event);
         // for all controllers in our application, make index and view
         // actions public, skipping the authentication check.
-        // $this->Authentication->addUnauthenticatedActions(['index', 'view']);
+        $this->Authentication->addUnauthenticatedActions(['index', 'view']);
     }
     /**
      * Initialization hook method.
